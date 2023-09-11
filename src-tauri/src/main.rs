3693faee3d73965281b943df7ede8892e19d1b24
kvoid <kianheitkamp@outlook.com>
2023-09-11 21:03:38 +0200

    minor changes & optimizations

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,10 @@
 #![allow(non_snake_case)]
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // no console
+
+const SHUFFLE_TWICE: bool = false;
 
 use rand::{seq::SliceRandom, Rng};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn generate_password(len: u16, use_nums: bool, use_special_characters: bool) -> String {
     const NUMS_WEIGHT: f32 = 0.25;
@@ -12,15 +12,10 @@ fn generate_password(len: u16, use_nums: bool, use_special_characters: bool) -> 
     const UPPER_WEIGHT: f32 = 0.25;
 
     // pools
-    const P_LOWER: &str = "abcdefghijklmnopqrstuvwxyz";
-    const P_UPPER: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const P_NUMS: &str = "0123456789";
-    const P_SPEC: &str = "*+-=:()[]&";
-
-    const P_LOWER_LEN: usize = P_LOWER.len();
-    const P_UPPER_LEN: usize = P_UPPER.len();
-    const P_NUMS_LEN: usize = P_NUMS.len();
-    const P_SPEC_LEN: usize = P_SPEC.len();
+    const LOWER: &str = "abcdefghijklmnopqrstuvwxyz";
+    const UPPER: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const NUMS: &str = "0123456789";
+    const SPEC: &str = "*+-=:()[]&";
 
     // generate password
 
@@ -29,32 +24,40 @@ fn generate_password(len: u16, use_nums: bool, use_special_characters: bool) -> 
     } else {
         0
     };
-    let len_spec = if use_special_characters { (len as f32 * SPEC_WEIGHT).ceil() as u16 } else { 0 };
+    let len_spec = if use_special_characters {
+        (len as f32 * SPEC_WEIGHT).ceil() as u16
+    } else {
+        0
+    };
     let len_upper = (len as f32 * UPPER_WEIGHT).ceil() as u16;
     let len_lower = len - len_nums - len_spec - len_upper;
 
-    let mut generated = String::new();
-
+    let mut generated = String::with_capacity(len as usize);
     let mut rng = rand::thread_rng();
 
     for _ in 0..len_nums {
-        generated.push((P_NUMS.chars().nth(rng.gen_range(0..P_NUMS_LEN))).unwrap());
+        generated.push((NUMS.chars().nth(rng.gen_range(0..NUMS.len()))).unwrap());
     }
     for _ in 0..len_spec {
-        generated.push(P_SPEC.chars().nth(rng.gen_range(0..P_SPEC_LEN)).unwrap());
+        generated.push(SPEC.chars().nth(rng.gen_range(0..SPEC.len())).unwrap());
     }
     for _ in 0..len_lower {
-        generated.push(P_LOWER.chars().nth(rng.gen_range(0..P_LOWER_LEN)).unwrap());
+        generated.push(LOWER.chars().nth(rng.gen_range(0..LOWER.len())).unwrap());
     }
     for _ in 0..len_upper {
-        generated.push(P_UPPER.chars().nth(rng.gen_range(0..P_UPPER_LEN)).unwrap());
+        generated.push(UPPER.chars().nth(rng.gen_range(0..UPPER.len())).unwrap());
     }
 
     let mut new = generated.clone().chars().collect::<Vec<char>>();
-    new.shuffle(&mut rng);
-    generated = new.iter().collect(); // shuffled
 
-    generated
+    drop(generated);
+
+    new.shuffle(&mut rng);
+    if SHUFFLE_TWICE {
+        new.shuffle(&mut rng);
+    }
+
+    new.iter().collect()
 }
 
 fn main() {
--- src/main.ts
+++ src/main.ts
@@ -4,50 +4,43 @@ import { invoke } from "@tauri-apps/api/tauri";
 let prev_len: number = 0;
 
 async function generatePassword() {
-    let passOutput: HTMLInputElement | null = document.querySelector("#pass-output");
-    let passLen: HTMLInputElement | null = document.querySelector("#pass-length");
-    let passLenOutput: HTMLInputElement | null = document.querySelector("#length-output")
-    let useNums: HTMLInputElement | null = document.querySelector("#use-numbers");
-    let useSChars: HTMLInputElement | null = document.querySelector("#use-schars");
+    let passOutput: HTMLInputElement = document.querySelector("#pass-output")!;
+    let passLen: HTMLInputElement = document.querySelector("#pass-length")!;
+    let useNums: HTMLInputElement = document.querySelector("#use-numbers")!;
+    let useSChars: HTMLInputElement = document.querySelector("#use-schars")!;
 
-    if (passOutput && passLen && passLenOutput && useNums && useSChars) {
-        passOutput.value = await invoke("generate_password",
-            { len: +passLen.value, useSpecialCharacters: useSChars.checked, useNums: useNums.checked, }
-        )
-    }
+    passOutput.value = await invoke("generate_password",
+        { len: +passLen.value, useSpecialCharacters: useSChars.checked, useNums: useNums.checked, }
+    )
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    let el: HTMLInputElement | null = document.querySelector("#pass-length");
-    if (el) el.oninput = () => {
-        let inp: HTMLInputElement | null = document.querySelector("#pass-length");
-        let out: HTMLInputElement | null = document.querySelector('#length-output');
+    let el: HTMLInputElement = document.querySelector("#pass-length")!;
+    el.oninput = () => {
+        let inp: HTMLInputElement = document.querySelector("#pass-length")!;
+        let out: HTMLInputElement = document.querySelector('#length-output')!;
 
-        if (inp && out) {
-            if (+inp.value === prev_len) return;
-            prev_len = +inp.value;
-            if (out) out.textContent = ('Length: ' + inp.value).padEnd(9, ' ');
-            generatePassword();
-        }
+        if (+inp.value === prev_len) return;
+        prev_len = +inp.value;
+        out.textContent = ('Length: ' + inp.value).padEnd(9, ' ');
+        generatePassword();
     }
 
-    let chN: HTMLInputElement | null = document.querySelector("#use-numbers");
-    let chS: HTMLInputElement | null = document.querySelector("#use-schars");
+    let chN: HTMLInputElement = document.querySelector("#use-numbers")!;
+    let chS: HTMLInputElement = document.querySelector("#use-schars")!;
 
-    if (chN && chS) {
-        chN.onchange = generatePassword;
-        chS.onchange = generatePassword;
-    }
+    chN.onchange = generatePassword;
+    chS.onchange = generatePassword;
 
-    let copyButton: HTMLInputElement | null = document.querySelector("#copy-button");
+    let copyButton: HTMLInputElement = document.querySelector("#copy-button")!;
 
-    if (copyButton) copyButton.onclick = () => {
-        let pass: HTMLInputElement | null = document.querySelector("#pass-output");
-        if (pass) clipboard.writeText(pass.value)
+    copyButton.onclick = () => {
+        let pass: HTMLInputElement = document.querySelector("#pass-output")!;
+        clipboard.writeText(pass.value)
     };
 
-    let regenButton: HTMLInputElement | null = document.querySelector("#regen-button");
-    if (regenButton) regenButton.onclick = generatePassword;
+    let regenButton: HTMLInputElement = document.querySelector("#regen-button")!;
+    regenButton.onclick = generatePassword;
 
     // run once on startup
     generatePassword();
